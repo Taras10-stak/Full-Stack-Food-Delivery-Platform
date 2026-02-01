a50c46ad7d28323d2dfcbb3ea482bd075a4e8143
Taras Sydor <65510045+Taras10-stak@users.noreply.github.com>
2026-02-01 18:36:26 +0200

    Update Cart.jsx

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -4,6 +4,11 @@ import Footer from '../../components/Footer/Footer'
 import CartPage from '../../components/CartPage/CartPage'
 
 const Cart = () => {
+
+    useEffect(() => {
+    window.scrollTo(0,0);
+    }, []);
+    
     return (
         <>
             <Navbar />
