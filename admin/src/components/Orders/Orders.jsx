817c2fd1e3f578022c790c16b398ffd1ef812b05
Taras Sydor <65510045+Taras10-stak@users.noreply.github.com>
2026-02-01 15:58:51 +0200

    Update Orders.jsx

--- admin/src/components/Orders/Orders.jsx
+++ admin/src/components/Orders/Orders.jsx
@@ -13,7 +13,7 @@ const Orders = () => {
     const fetchOrders = async () => {
       try {
         const response = await axios.get(
-          'http://localhost:4000/api/orders/getall',
+          'https://full-stack-food-delivery-platform-backend.onrender.com/api/orders/getall',
           {
             headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
           }
