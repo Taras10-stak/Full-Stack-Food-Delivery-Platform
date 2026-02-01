17c4839cea031619dd878b0d5b4d3858109d4de2
Taras Sydor <65510045+Taras10-stak@users.noreply.github.com>
2026-02-01 19:10:04 +0200

    Update Menu.jsx

--- frontend/src/pages/Menu/Menu.jsx
+++ frontend/src/pages/Menu/Menu.jsx
@@ -4,9 +4,7 @@ import OurMenu from '../../components/OurMenu/OurMenu'
 import Footer from '../../components/Footer/Footer'
 const Menu = () => {
 
-    useEffect(() => {
-    window.scrollTo(0,0);
-    }, []);
+    
     
     return (
         <>
