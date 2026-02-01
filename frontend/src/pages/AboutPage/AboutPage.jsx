a4a45e89a7b06c50774c2be37bcf2d7a91c2a36d
Taras Sydor <65510045+Taras10-stak@users.noreply.github.com>
2026-02-01 19:08:33 +0200

    Update AboutPage.jsx

--- frontend/src/pages/AboutPage/AboutPage.jsx
+++ frontend/src/pages/AboutPage/AboutPage.jsx
@@ -4,9 +4,7 @@ import About from "../../components/About/About"
 import Footer from '../../components/Footer/Footer'
 const AboutPage = () => {
 
-    useEffect(() => {
-    window.scrollTo(0,0);
-    }, []);
+    
     
     return (
         <>
