c49cee03b06fe4465e9e3d27b1c69d0b961cfeb5
Taras Sydor <65510045+Taras10-stak@users.noreply.github.com>
2026-02-01 15:52:10 +0200

    Update AddItems.jsx

--- admin/src/components/AddItems/AddItems.jsx
+++ admin/src/components/AddItems/AddItems.jsx
@@ -54,7 +54,7 @@ const AddItems = () => {
         payload.append(key, val);
       });
       const res = await axios.post(
-        'http://localhost:4000/api/items',
+        'https://full-stack-food-delivery-platform-backend.onrender.com/api/items',
         payload,
         { headers: { 'Content-Type': 'multipart/form-data' } }
       );
