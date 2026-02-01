395207e0b866c04b8cfefb12298fb1a4531a6d17
Taras Sydor <65510045+Taras10-stak@users.noreply.github.com>
2026-02-01 18:35:47 +0200

    Update AboutPage.jsx

--- frontend/src/pages/AboutPage/AboutPage.jsx
+++ frontend/src/pages/AboutPage/AboutPage.jsx
@@ -3,6 +3,11 @@ import Navbar from '../../components/Navbar/Navbar'
 import About from "../../components/About/About"
 import Footer from '../../components/Footer/Footer'
 const AboutPage = () => {
+
+    useEffect(() => {
+    window.scrollTo(0,0);
+    }, []);
+    
     return (
         <>
             <Navbar />
